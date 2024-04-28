43ca6d981b133aeee529dd53d79ece5e2f6e996a
Jack Tam <jtam64@my.bcit.ca>
2024-04-28 10:35:33 -0700

    added centralized logging

--- medicine_store/app.py
+++ medicine_store/app.py
@@ -18,7 +18,7 @@ import logging.config
 with open("app_conf.yml", "r") as f:
     app_config = yaml.safe_load(f.read())
 
-with open("log_conf.yml", "r") as f:
+with open("/logs/log_conf.yml", "r") as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 

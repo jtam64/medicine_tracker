f102e4d27cd2d836a264f86ff95e104b944e0ab0
Jack Tam <jtam64@my.bcit.ca>
2024-04-28 10:38:12 -0700

    fixed centralized logging

--- daily_updater/app.py
+++ daily_updater/app.py
@@ -14,7 +14,7 @@ import yaml
 with open("app_conf.yml", "r") as f:
     app_config = yaml.safe_load(f.read())
 
-with open("/logs/log_conf.yml", "r") as f:
+with open("log_conf.yml", "r") as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
